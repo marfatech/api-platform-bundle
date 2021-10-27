2a4da4007d491013b627110f5ecb045b3b6155dd
apogadaev <apogadaev@marfa-tech.com>
2021-10-27 16:36:17 +0300

    DEV-16 extension case fix

--- DependencyInjection/MarfatechApiPlatformExtension.php
+++ DependencyInjection/MarfatechApiPlatformExtension.php
@@ -18,7 +18,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class MarfaTechApiPlatformExtension extends Extension
+class MarfatechApiPlatformExtension extends Extension
 {
     /**
      * {@inheritdoc}
