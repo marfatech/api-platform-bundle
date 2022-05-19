5973d0e44d8b0ceddde48b7c426a614df23427b9
ekuznetsov <ekuznetsov@marfa-tech.com>
2022-05-19 11:04:06 +0300

    DEV-49 ### Changed - Set alias `marfa_tech_api_platform.factory.api_dto` of the service `MarfaTech\Bundle\ApiPlatformBundle\Factory\ApiDtoFactory` at compiler pass cause BC.

--- DependencyInjection/Compiler/OptionsResolverFactoryCompiler.php
+++ DependencyInjection/Compiler/OptionsResolverFactoryCompiler.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace MarfaTech\Bundle\ApiPlatformBundle\DependencyInjection\Compiler;
 
 use Linkin\Bundle\SwaggerResolverBundle\Factory\SwaggerResolverFactory;
+use MarfaTech\Bundle\ApiPlatformBundle\Factory\ApiDtoFactory;
+use MarfaTech\Bundle\ApiPlatformBundle\Factory\ApiDtoResolverFactory;
 use MarfaTech\Bundle\ApiPlatformBundle\Factory\OptionsResolverFactory;
 use MarfaTech\Bundle\ApiPlatformBundle\Factory\ResolverFactoryInterface;
 use MarfaTech\Bundle\ApiPlatformBundle\Factory\SwaggerResolverFactoryWrapper;
@@ -27,11 +29,15 @@ class OptionsResolverFactoryCompiler implements CompilerPassInterface
 
             $container->setAlias(ResolverFactoryInterface::class, SwaggerResolverFactoryWrapper::class);
             $container->setDefinition(SwaggerResolverFactoryWrapper::class, $swaggerResolverFactoryWrapper);
+
+            $container->setAlias('marfa_tech_api_platform.factory.api_dto', ApiDtoFactory::class);
         } else {
             $optionsResolverFactory = new Definition(OptionsResolverFactory::class);
 
             $container->setAlias(ResolverFactoryInterface::class, OptionsResolverFactory::class);
             $container->setDefinition(OptionsResolverFactory::class, $optionsResolverFactory);
+
+            $container->setAlias('marfa_tech_api_platform.factory.api_dto', ApiDtoResolverFactory::class);
         }
     }
 }
